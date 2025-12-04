c0cd9bb862d6a5012cd8143c25c272683974e9fd
TariqRash <tariq@hostworksa.com>
2025-12-04 16:11:32 +0000

    Add max interger search with simple loop

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    """Find the biggest integer of a list."""
+    if not my_list:
+        return None
+    max_val = my_list[0]
+    for number in my_list[1:]:
+        if number > max_val:
+            max_val = number
+    return max_val
