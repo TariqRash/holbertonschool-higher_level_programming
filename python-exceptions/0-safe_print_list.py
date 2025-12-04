490bf36f1962c95ddbeaa05a000f542a19dbeead
TariqRash <tariq@hostworksa.com>
2025-12-04 17:26:08 +0000

    fix task0 safe lst print, now pass all chcks i hope

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    try:
-        for i in range(x):
-            try:
-                print("{}".format(my_list[i]), end="")
-                count += 1
-            except Exception:
-                break
-    finally:
-        print()
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            count += 1
+        except Exception:
+            break
+    print()
     return count
