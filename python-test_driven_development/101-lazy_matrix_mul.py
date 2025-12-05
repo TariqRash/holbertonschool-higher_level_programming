314246d8ac1a1f7985ca84d731b15624d060009f
TariqRash <tariq@hostworksa.com>
2025-12-05 19:56:17 +0000

    simplify to pure numpy matmul for checker compatibility

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -17,4 +17,4 @@ def lazy_matrix_mul(m_a, m_b):
     Returns:
         A NumPy array representing the product of m_a and m_b
     """
-    return np.matmul(m_a, m_b)
+    return (np.matmul(m_a, m_b))
