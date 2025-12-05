e2f012ef3c3f7ff5cce5dfb64b3bb7eaf16a63a7
TariqRash <tariq@hostworksa.com>
2025-12-05 19:08:27 +0000

    fix pycodestyle whitespace issues in matrix and text functions

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -17,7 +17,7 @@ def text_indentation(text):
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    
+
     i = 0
     while i < len(text):
         print(text[i], end="")
