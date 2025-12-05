2c24c4db27ab3a95868ecc9794bb1af2d14d8fa9
TariqRash <tariq@hostworksa.com>
2025-12-05 19:11:00 +0000

    remove all trailing whitespaces from matrix divided file

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -23,28 +23,28 @@ def matrix_divided(matrix, div):
         ZeroDivisionError: If div is zero
     """
     error_msg = "matrix must be a matrix (list of lists) of integers/floats"
-    
+
     if not isinstance(matrix, list) or len(matrix) == 0:
         raise TypeError(error_msg)
-    
+
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
-    
+
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    
+
     row_size = None
     for row in matrix:
         if not isinstance(row, list) or len(row) == 0:
             raise TypeError(error_msg)
-        
+
         if row_size is None:
             row_size = len(row)
         elif len(row) != row_size:
             raise TypeError("Each row of the matrix must have the same size")
-        
+
         for element in row:
             if not isinstance(element, (int, float)):
                 raise TypeError(error_msg)
-    
+
     return [[round(element / div, 2) for element in row] for row in matrix]
