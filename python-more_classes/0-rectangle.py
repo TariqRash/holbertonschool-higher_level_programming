4317eb26fc743a2bdc4d041e0bda20340b7e63b8
tariqrash <tariq@hostworksa.com>
2025-12-13 16:25:27 +0000

    Task 0: Create empty Rectangle class
    Added basic class definiton for rectangle

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module that defines an empty Rectangle class.
+
+This module provides a basic Rectangle class as an introduction to
+more advanced object-oriented programming concepts in Python.
+"""
+
+
+class Rectangle:
+    """Represents a rectangle.
+
+    This is an empty class that serves as the foundation for building
+    a complete Rectangle implementation with properties and methods.
+    """
+    pass
