e3bf135731e0aca2948c8f32efc2dd23a3716b6e
Tariq Rashid <tariq@hostworksa.com>
2026-01-16 23:02:20 +0000

    Cities table with FK to states

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- Creates table cities with foreign key to states
+-- state_id references states(id)
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
