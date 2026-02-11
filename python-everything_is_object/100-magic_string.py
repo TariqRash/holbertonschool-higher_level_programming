51f48e5c17ebbada36dcf4649ad3a13c9bb99379
TariqRash <tariq@hostworksa.com>
2026-02-11 19:10:00 +0300

    Fix Task 30: magic_string exactly 4 lines

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.strings = getattr(magic_string, 'strings', [])
-    magic_string.strings.append("BestSchool")
-    return ", ".join(magic_string.strings)
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ", ".join(["BestSchool"] * magic_string.n)
